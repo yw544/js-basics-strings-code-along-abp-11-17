074d07cef898156f3bb3e4479bee833f29bec220
York Wang <yw544@nyu.edu>
2017-11-21 08:20:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const currentUser = "Grace Hopper";
 
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
 
-const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + .toUpperCase(currentUser) + "!";
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + toUpperCase(currentUser) + "!";
