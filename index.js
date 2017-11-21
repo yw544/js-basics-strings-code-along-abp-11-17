dafe8c3c2eff220f983e0dc1e76d83ad42300a70
York Wang <yw544@nyu.edu>
2017-11-21 08:18:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // Write your code in this file!
 const currentUser = "Grace Hopper";
 
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!"
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + "!";
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser + "!";
